eb54a269ba7a5d9c0388d9f78071ca7b64b569d7
liangzhuang <397548123@qq.com>
2019-04-23 21:01:48 +0800

    无

---
+++ src/five/hello.java
@@ -0,0 +1,14 @@
+package five;
+
+/**
+ * @ClassName: hello
+ * @Description: TODO
+ * @Author: 梁壮
+ * @Date: 2019/4/23 20:59
+ * @Version: 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("我地肚子又大又圆，肚子里面都是麻辣香锅。。。。");
+    }
+}
