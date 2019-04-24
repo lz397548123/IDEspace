905aa5ca3bc0af73c486c78367836b27f111dd0e
梁壮 <397548123@qq.com>
2019-04-24 08:23:03 +0800

    无

---
+++ src/five/hello2.java
@@ -0,0 +1,14 @@
+package five;
+
+/**
+ * @ClassName: hello2
+ * @Description: TODO
+ * @Author: 梁壮
+ * @Date: 2019/4/24 8:21
+ * @Version: 1.0
+ */
+public class hello2 {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
