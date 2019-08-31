65a2ca03a5207f2ae63c1ae9f2619ffa39d25b01
lz397548123 <397548123@qq.com>
2019-08-31 09:39:08 +0800

    无

--- src/five/ExceptionTest.java
+++ src/five/ExceptionTest.java
@@ -9,6 +9,7 @@ package five;
  */
 public class ExceptionTest {
     public static void main(String[] args) {
+        //Test
         int x = 16;
         int y = 0;
         int z[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
